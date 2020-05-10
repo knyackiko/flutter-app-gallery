a8050e14b8efda41c70d4c9a14a0be4b0038babd
kyklades <33259005+kyklades@users.noreply.github.com>
2020-05-10 22:19:33 +0900

    複数枚写真がある場合は、右上にページを表示するようにした

--- infinite_scroll_demo_app/lib/ui/post/post_pictures.dart
+++ infinite_scroll_demo_app/lib/ui/post/post_pictures.dart
@@ -9,13 +9,6 @@ class PostPictures extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final images = pictures
-        .map((picture) => Image.asset(
-              picture.path,
-              fit: BoxFit.fill,
-            ))
-        .toList();
-
     final width = MediaQuery.of(context).size.width;
 
     return Container(
@@ -27,8 +20,45 @@ class PostPictures extends StatelessWidget {
       height: width,
       child: PageView(
         controller: _pageController,
-        children: images,
+        children: _images(),
       ),
     );
+  }
+
+  List<Widget> _images() {
+    if (pictures.length == 1) {
+      return pictures
+          .map((picture) => Image.asset(
+                picture.path,
+                fit: BoxFit.fill,
+              ))
+          .toList();
+    }
+
+    return pictures.asMap().entries.map((entry) {
+      final picture = entry.value;
+      final index = entry.key;
+      return Stack(
+        children: [
+          Image.asset(
+            picture.path,
+            fit: BoxFit.fill,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.7),
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+              ),
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.symmetric(horizontal: 13, vertical: 8),
+              child: Text('${index + 1}/${pictures.length}',
+                  style: TextStyle(color: Colors.white)),
+            ),
+          )
+        ],
+      );
+    }).toList();
   }
 }
